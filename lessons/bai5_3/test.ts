a3769e8660ae208e5978f77e225b83fb3adf05c0
hoangnahd <135134713+hoangnahd@users.noreply.github.com>
2025-10-11 19:56:16 +0700

    Update test.ts

--- lessons/bai5_3/test.ts
+++ lessons/bai5_3/test.ts
@@ -7,7 +7,7 @@ async function main() {
     "function getCount() public view returns (uint)",
     "function increment() public"
   ];
-  const contractAddress = "0x41d0Ad4E6227062B59CA673B418Fc00E3A2d10Cf"; // Replace with your contract address
+  const contractAddress = "0xDFe70ae94585dCE94685de4a021207215d2307cE"; // Replace with your contract address
 
   const contract = new ethers.Contract(contractAddress, abi, provider);
 
